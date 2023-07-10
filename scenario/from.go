cf9c47cf904587c9ebb109d57413453b5979214a
Jay Pipes <jaypipes@gmail.com>
2023-07-10 16:03:07 -0400

    export expandWithFixedDoubleDollar

--- scenario/from.go
+++ scenario/from.go
@@ -5,19 +5,12 @@
 package scenario
 
 import (
-	"fmt"
 	"io"
 	"io/ioutil"
-	"os"
-	"strings"
 
+	"github.com/jaypipes/gdt-core/parse"
 	gdttypes "github.com/jaypipes/gdt-core/types"
 	"gopkg.in/yaml.v3"
-)
-
-const (
-	// hopefully nobody actually has an environment variable with this key!
-	dollarSignReplacementToken = "oiuqdfjhaso7t213041"
 )
 
 // FromReader parses the supplied io.Reader and returns a Scenario representing
@@ -40,21 +33,10 @@ func FromBytes(
 	mods ...ScenarioModifier,
 ) (gdttypes.Runnable, error) {
 	s := New(mods...)
-	expanded := expandWithFixedDoubleDollar(string(contents))
+	expanded := parse.ExpandWithFixedDoubleDollar(string(contents))
 	if err := yaml.Unmarshal([]byte(expanded), s); err != nil {
 		return nil, err
 	}
 
 	return s, nil
-}
-
-// expandWithFixedDoubleDollar expands the given string using os.ExpandEnv,
-// however unlike the default behaviour of replacing a string "$$VALUE" with
-// "VALUE", it replaces the "$$" witha single "$". This allows test authors to
-// use the dollar symbol in their test contents (they need to escape with
-// '$$').
-func expandWithFixedDoubleDollar(subject string) string {
-	os.Setenv(dollarSignReplacementToken, "$")
-	replaceStr := fmt.Sprintf("${%s}", dollarSignReplacementToken)
-	return os.ExpandEnv(strings.Replace(subject, "$$", replaceStr, -1))
 }
